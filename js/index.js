7b9fb4b668f8b36775b1ad47d6f05e26b8070e72
Tomer Ovadia <o.tomerr@gmail.com>
2021-01-03 19:22:12 +0200

    :recycle: Adjust dino image

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-$(function () {
-  const $img = $('#randomImg');
-  const images = ['contributions.gif', 'dev.gif', 'dino.gif', 'nothing.gif', 'octo.gif'];
-  const image = images[Math.floor(Math.random() * images.length)];
-  $img.attr('src', 'img/' + image);
-})
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+}
+
+window.ready(function () {
+  let i = 0;
+  const basicMsg = "Don't try again";
+  let msg = basicMsg;
+  document.getElementById("dinoButton").onclick = function () {
+    if (i > 0) {
+      msg = basicMsg + ", I mean it.";
+    }
+    alert(msg);
+    i++;
+  };
+});
