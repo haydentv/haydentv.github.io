97a194d45fbdee8b1fa08046a2c4a33ecf8463af
Tomer Ovadia <o.tomerr@gmail.com>
2021-01-03 18:52:40 +0200

    :sparkles: Random image

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(function () {
-  alert('a');
+  const $img = $('#randomImg');
+  const images = ['contributions.gif', 'dev.gif', 'dino.gif', 'nothing.gif', 'octo.gif'];
+  const image = images[Math.floor(Math.random() * images.length)];
+  $img.src = 'img/' + image;
 })
